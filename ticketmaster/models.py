55fb48f8b725046c3fdb927f21e5fcc398753fd1
Raghubir Bhandari <bhandariraghubir23@gmail.com>
2023-12-04 19:10:44 -0500

    updated

--- ticketmaster/models.py
+++ ticketmaster/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
-class Entry(models.Model):
-    events = models.CharField(max_length=30)
-    city = models.CharField(max_length=30)
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Comment(models.Model):
+    # User model is created by Django. Instead of creating our own User model, we can simply use Django's user model.
+    # the following line create a ForeignKey in your table
+    # Since each user can have many comments, 1-to-many relationship is needed.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- ticketmaster/views.py
+++ ticketmaster/views.py
@@ -3,72 +3,96 @@ from django.http import HttpResponse
 import requests
 from datetime import datetime
 from django.contrib import messages
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-    # if the request method is a post
     if request.method == 'POST':
-        # get the search term and location
         events = request.POST['events']
         city = request.POST['city']
 
-        # Check if either events or city is empty
         if not events and not city:
             messages.info(request, 'Please enter both events name and location.')
-            # redirect user to the index page
             return redirect('ticketmaster-index')
 
-        # call function get_events() to get the data from the API
         total_events = get_events(events, city)
-        print(total_events)
 
-        # If the request to fetch data from ticketmaster was unsuccessful or returned None
         if total_events is None:
-            # Set up an error message using Django's message utility to inform the user
             messages.info(request, 'The server encountered an issue while fetching data. Please try again later.')
-            # redirect user to the index page
+            return redirect('ticketmaster-index')
+        elif 'errors' in total_events:
+            messages.info(request, 'No results found for the entered search terms.')
             return redirect('ticketmaster-index')
         else:
-            # Store each user's information in a variable
-            users = total_events['results']
-
-            # Initialize an empty list to store user data
             user_list = []
 
-            for user in users:
-                # Extract relevant information from the user dictionary
-                venueName = user['venueName']
-                venueCity = user['venueCity']
-                venueState = user['venueState']
-                venueAddress = user['venueAddress']
-                picture = user['picture']
-                ticketLink = user.url
+            if '_embedded' in total_events and 'events' in total_events['_embedded']:
+                for event in total_events['_embedded']['events']:
+                    event_name = event['name']
+                    venue_name = event['_embedded']['venues'][0]['name']
+                    venue_address = event['_embedded']['venues'][0]['address']['line1']
+                    venue_state = event['_embedded']['venues'][0]['state']['name']
+                    venue_city = event['_embedded']['venues'][0]['city']['name']
+                    ticket_link = event['url']
 
-                # Create a new dictionary to store user details
-                user_details = {
-                    'venueName': venueName,
-                    'venueCity': venueCity,
-                    'venueState': venueState,
-                    'venueAddress': venueAddress,
-                    'picture': picture,
-                    'ticketLink': ticketLink
-                }
+                    # Call the method to get the largest image URL
+                    largest_image_url = get_largest_image(event['images'])
 
-                # Append the user details dictionary to the user_list
-                user_list.append(user_details)
+                    if 'dateTime' in event['dates']['start']:
+                        event_datetime_str = event['dates']['start']['dateTime']
+                        event_datetime = datetime.fromisoformat(event_datetime_str.replace("Z", "+00:00"))
 
-            # Create a context dictionary with the user_list and render the 'index.html' template
-            context = {'users': user_list}
+                        # Format the event date and time to human-readable formats
+                        formatted_event_date = event_datetime.strftime('%a, %b %d, %Y')
+                        formatted_event_time = event_datetime.strftime('%I:%M %p')
+                    else:
+                        # Handle the case where 'dateTime' key is not present
+                        registration_date = "Date not available"
+                        formatted_event_time = "Time not available"
+
+                    # Create a new dictionary to store user details
+                    user_details = {
+                        'eventName': event_name,
+                        'venueName': venue_name,
+                        'venueAddress': venue_address,
+                        'venueCity': venue_city,
+                        'venueState': venue_state,
+                        'ticketLink': ticket_link,
+                        'imageUrl': largest_image_url,
+                        'eventDate': formatted_event_date,
+                        'eventTime': formatted_event_time,
+                    }
+                    user_list.append(user_details)
+
+                # Calculate the total number of events
+                total_nums_of_events = len(user_list)
+            else:
+                messages.info(request, 'No event found.')
+                total_nums_of_events = 0
+
+            context = {'users': user_list, 'total_events': total_nums_of_events}
             return render(request, 'ticketmaster/myPageTicketmaster.html', context)
-
-    # just render the page without sending/passing any context to the template It
     return render(request, 'ticketmaster/myPageTicketmaster.html')
 
 
-def get_events(events, city):
-    print('test')
+# This Function to get the largest pic from of the event based on the height and width
+def get_largest_image(images):
+    # Initialize with the first image
+    largest_image = None
+    largest_size = images[0]['width'] * images[0]['height']
 
+    for current_image in images[1:]:
+        current_size = current_image['width'] * current_image['height']
+
+        # Compare based on size
+        if current_size > largest_size:
+            largest_image = current_image['url']
+            largest_size = current_size
+    return largest_image
+
+
+def get_events(events, city):
     try:
         apikey = 'BSQwvGEqBVyuq8qtKajfgDTwfuufWVUX'
         url = "https://app.ticketmaster.com/discovery/v2/events.json"
@@ -81,7 +105,6 @@ def get_events(events, city):
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
-        print(data)
         return data
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
