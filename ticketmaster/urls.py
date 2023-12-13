1c559e5fe341a01d0fbe6ce8598b9b71186d1a36
Raghubir Bhandari <bhandariraghubir23@gmail.com>
2023-12-13 18:21:45 -0500

    updated

--- ticketmaster/urls.py
+++ ticketmaster/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
     #path('', views.ticketmaster, name="ticketmaster"),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('update/<int:_commentid>', views.update_comment, name='update_product'),
     path('delete/<int:product_id>', views.delete_comment, name='delete_product'),
     path('search/', views.search_comment, name='search_product'),
+    path('get_comments/', views.get_comments, name='get_comments'),
 ]
